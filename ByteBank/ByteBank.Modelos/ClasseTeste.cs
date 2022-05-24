cd367bb6dddcb6da4849b22548feceb0a2b0c09e
Jonathan Ferreira <jhouus33@gmail.com>
2022-05-23 23:25:48 -0300

    Add projeto Byte com aprendizado sobre modificadores de acesso

---
+++ ByteBank/ByteBank.Modelos/ClasseTeste.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    internal class ClasseTeste
+    {
+        ModificadoresTeste teste = new ModificadoresTeste();
+        public ClasseTeste()//implicita internal
+        {
+            teste.MetodoPublico();
+            teste.MetodoInternal();// fora da classe porem no mesmo projeto
+
+            // teste.MetodoPrivate(); // visivem apenas na classe ModificadoresTeste
+            // teste.MetodoProtected(); // visivel somente na classe ModificadoresTeste ou na classe DerivadaTeste que herda de ModificadoresTeste
+
+        }
+
+    }
+
+    class DerivadaTeste : ModificadoresTeste
+    {
+        public DerivadaTeste()
+        {
+            MetodoProtected();
+        }
+
+    }
+
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico() { }
+        private void MetodoPrivate() { }
+        internal void MetodoInternal() { }
+        protected void MetodoProtected() { }
+    }
+}
---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,17 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(001, 123456);
+        }
+    }
+}
