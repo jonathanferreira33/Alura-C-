fbb39906e4f24ca3cc3c350b7355b1a53cfd05b4
Jonathan Ferreira <jhouus33@gmail.com>
2022-05-31 01:34:04 -0300

    funcionamento de DateTime e Humanizer

--- ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
+++ ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBank.Modelos;
+using Humanizer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,34 @@ namespace ByteBank.SistemaInterno
             ContaCorrente conta = new ContaCorrente(0001, 654321);
             Console.WriteLine(conta.Saldo);
 
+            DateTime dataFim = new DateTime(2022, 06, 30);
+            DateTime dataAtual = DateTime.Now; // propridade estatica
+
+
+            TimeSpan diferenca = dataFim - dataAtual;
+            string msg = "vencimento em: " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(dataAtual);
+            Console.WriteLine(dataFim);
+            Console.WriteLine(msg);
+
+
+
             Console.ReadLine();
+        }
+
+        //forma suspeita
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        {
+            if (timeSpan.Days > 1)
+                return timeSpan.Days + "em dias";
+            else if (timeSpan.Hours > 1)
+                return timeSpan.Hours + "em horas";
+            else if (timeSpan.Minutes >= 1)
+                return timeSpan.Minutes + "em minutos";
+            else
+                return "atingido!";
+
         }
     }
 }
